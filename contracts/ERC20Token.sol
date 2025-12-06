b73356d7553bef813a3bade8760f6955f7a4e5af
MyronZhang <myronzhangweb3@gmail.com>
2025-12-06 21:22:52 +0800

    fix: test token mint

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract ERC20Token is ERC20, Ownable {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) Ownable(msg.sender) {}
 
-    function mint(address to, uint256 amount) external onlyOwner {
+    function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
 }
